308790e39617014d5f54b89fc2b91e2eafb2f602
Mohammed Yahya <yahyamd97@gmail.com>
2026-01-20 11:32:31 +0530

    fixed config for vercel deployment

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,9 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  outputFileTracingRoot: path.join(__dirname, '..'),
+  turbopack: {
+    root: __dirname, // Set frontend directory as the workspace root
+  },
   images: {
     remotePatterns: [
       {
